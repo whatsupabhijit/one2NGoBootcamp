e22fdf47d4ee220101c74e21b7baf0b1eb9bf5fb
whatsupabhijit <tech.abhijit.dutta@gmail.com>
2024-01-07 15:49:57 +0530

    Add basic famous hello  world and user input processing in go

---
+++ user-input.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var fName string
+	fmt.Println("Enter First Name: ")
+	fmt.Scanln(&fName)
+
+
+	var lName string
+	fmt.Println("Enter Last Name: ")
+	fmt.Scanln(&lName)
+
+	fmt.Println(fName + " " + lName)
+}
